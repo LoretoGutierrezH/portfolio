a505e3e2010b17f5bac4449228bafa262fb28116
LoretoGutierrezH <loretocgh@gmail.com>
2020-09-09 23:27:19 -0300

    círculos de svg con cambio de clase a active mediante evento click

--- script.js
+++ script.js
@@ -7,28 +7,52 @@ const developmentCircle = document.querySelector('.development');
 const contentParagraph = document.querySelector('.aboutMe-content');
 
 translationCircle.addEventListener('click', () => {
+  translationCircle.classList.add('active-svg-circle');
+  rubyCircle.classList.remove('active-svg-circle');
+  udemyCircle.classList.remove('active-svg-circle');
+  laboratoriaCircle.classList.remove('active-svg-circle');
+  developmentCircle.classList.remove('active-svg-circle');
+
   contentParagraph.textContent = "";
-  contentParagraph.classList.add('slideInFromLeft');
   contentParagraph.textContent = "Soy traductora inglés-español de profesión, pero hace algún tiempo tomé la decisión de cambiar radicalmente de rubro, esta vez al área de la tecnología.Poseo una base sólida de HTML5, CSS3, Vanilla Javascript y React, y me interesan tanto las tecnologías de Front - End como las de Back - End.A nivel personal, soy adicta al ceviche, me encantan los libros, las películas, los animales y la naturaleza y tengo un lazo inquebrantable con mi taza de café";
 })
 
 rubyCircle.addEventListener('click', () => {
+  rubyCircle.classList.add('active-svg-circle');
+  translationCircle.classList.remove('active-svg-circle');
+  udemyCircle.classList.remove('active-svg-circle');
+  laboratoriaCircle.classList.remove('active-svg-circle');
+  developmentCircle.classList.remove('active-svg-circle');
   contentParagraph.textContent = "";
-  contentParagraph.classList.add('slideInFromLeft');
   contentParagraph.textContent = "Información sobre bootcamp Ruby on Rails";
 })
 
 udemyCircle.addEventListener('click', () => {
+  udemyCircle.classList.add('active-svg-circle');
+  translationCircle.classList.remove('active-svg-circle');
+  rubyCircle.classList.remove('active-svg-circle');
+  laboratoriaCircle.classList.remove('active-svg-circle');
+  developmentCircle.classList.remove('active-svg-circle');
   contentParagraph.textContent = "";
   contentParagraph.textContent = "Información sobre cursos Udemy";
 })
 
 laboratoriaCircle.addEventListener('click', () => {
+  laboratoriaCircle.classList.add('active-svg-circle');
+  translationCircle.classList.remove('active-svg-circle');
+  rubyCircle.classList.remove('active-svg-circle');
+  udemyCircle.classList.remove('active-svg-circle');
+  developmentCircle.classList.remove('active-svg-circle');
   contentParagraph.textContent = "";
   contentParagraph.textContent = "Información sobre bootcamp Laboratoria";
 })
 
 developmentCircle.addEventListener('click', () => {
+  developmentCircle.classList.add('active-svg-circle');
+  translationCircle.classList.remove('active-svg-circle');
+  rubyCircle.classList.remove('active-svg-circle');
+  udemyCircle.classList.remove('active-svg-circle');
+  laboratoriaCircle.classList.remove('active-svg-circle');
   contentParagraph.textContent = "";
   contentParagraph.textContent = "Información sobre mi inicio como Front-End Developer";
 })
@@ -57,23 +81,26 @@ skillsNavbarArray.forEach((option) => {
       event.target.classList.add('active-option')
       notions.classList.remove('active-option');
       interest.classList.remove('active-option');
-      strongBasicsContent.classList.remove('hidden');
-      notionsContent.classList.add('hidden');
-      interestContent.classList.add('hidden');
+
+      strongBasicsContent.classList.replace('hidden', 'active');
+      notionsContent.classList.replace('active', 'hidden');
+      interestContent.classList.replace('active', 'hidden');
     } else if (event.target.innerText === "Nociones") {
       event.target.classList.add('active-option');
       strongBasics.classList.remove('active-option');
       interest.classList.remove('active-option');
-      notionsContent.classList.remove('hidden');
-      strongBasicsContent.classList.add('hidden');
-      interestContent.classList.add('hidden');
+
+      notionsContent.classList.replace('hidden', 'active');
+      strongBasicsContent.classList.replace('active', 'hidden');
+      interestContent.classList.replace('active', 'hidden');
     } else {
       event.target.classList.add('active-option');
       strongBasics.classList.remove('active-option');
       notions.classList.remove('active-option');
-      interestContent.classList.remove('hidden');
-      notionsContent.classList.add('hidden');
-      strongBasicsContent.classList.add('hidden');
+
+      interestContent.classList.replace('hidden', 'active');
+      notionsContent.classList.replace('active', 'hidden');
+      strongBasicsContent.classList.replace('active', 'hidden');
     }
 
   })
